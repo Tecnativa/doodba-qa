d8d88364d5ab0ad27a4499ba67c123af49636bcb
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-01-14 09:41:31 +0100

    Fix travis build

--- tests/scaffoldings/0/custom/src/private/addon_1/hooks.py
+++ tests/scaffoldings/0/custom/src/private/addon_1/hooks.py
@@ -1,1 +1,1 @@
-# This is a very long line that usually would fail any linter that follows PEP 8
+# This is a very long line that usually would fail OCA's MQT linter, which expect a maximum of 88 chars in the line length because it matches what black does
